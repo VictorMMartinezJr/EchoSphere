bd99efcf9f9ab53313959eb351c2ab19b730267d
Victor <victorm.martinezjr@outlook.com>
2025-10-14 21:32:16 -0400

    create Song document

---
+++ backend/EchoSphere/src/main/java/in/victormartinezjr/echosphere/document/Song.java
@@ -0,0 +1,26 @@
+package in.victormartinezjr.echosphere.document;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Song {
+    @Id
+    @JsonProperty("_id")
+    private String id;
+
+    private String name;
+    private String albumName;
+    private String image;
+    private String file;
+    private String duration;
+}
