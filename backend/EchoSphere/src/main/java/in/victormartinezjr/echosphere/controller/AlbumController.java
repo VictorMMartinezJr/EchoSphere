3c368b913a9cbb48c0a8d1af8f7edf2ab79cbc83
Victor <victorm.martinezjr@outlook.com>
2025-10-14 20:53:22 -0400

    add getAlbums GetMapping and deleteAlbum DeleteMapping to AlbumController

--- backend/EchoSphere/src/main/java/in/victormartinezjr/echosphere/controller/AlbumController.java
+++ backend/EchoSphere/src/main/java/in/victormartinezjr/echosphere/controller/AlbumController.java
@@ -1,17 +1,13 @@
 package in.victormartinezjr.echosphere.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import in.victormartinezjr.echosphere.dto.AlbumListResponse;
 import in.victormartinezjr.echosphere.dto.AlbumRequest;
 import in.victormartinezjr.echosphere.service.AlbumService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
@@ -31,4 +27,29 @@ public class AlbumController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
     }
+
+    @GetMapping
+    public ResponseEntity<?> listAlbums() {
+        try {
+            return ResponseEntity.ok(albumService.getAllAlbums());
+        } catch (Exception e) {
+            return ResponseEntity.ok(new AlbumListResponse(false, null));
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteAlbum(@PathVariable String id) {
+        try {
+            Boolean deleted = albumService.deleteAlbum(id);
+            if (deleted) {
+                return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+            } else {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+    }
+
+
 }
