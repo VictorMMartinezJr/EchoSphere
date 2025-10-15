f3a656456ac472177c4b76a0339be9517a34511d
Victor <victorm.martinezjr@outlook.com>
2025-10-14 21:36:46 -0400

    create SongService with addSong method

---
+++ backend/EchoSphere/src/main/java/in/victormartinezjr/echosphere/service/SongService.java
@@ -0,0 +1,52 @@
+package in.victormartinezjr.echosphere.service;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import in.victormartinezjr.echosphere.document.Song;
+import in.victormartinezjr.echosphere.dto.SongRequest;
+import in.victormartinezjr.echosphere.repository.SongRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class SongService {
+    private final SongRepository songRepository;
+    private final Cloudinary cloudinary;
+
+    public Song addSong(SongRequest songRequest) throws IOException {
+        Map<String, Object> audioUploadResult = cloudinary.uploader()
+                .upload(songRequest.getAudioFile().getBytes(), ObjectUtils.asMap("resource_type", "video"));
+
+        Map<String, Object> imageUploadResult = cloudinary.uploader()
+                .upload(songRequest.getImageFile().getBytes(), ObjectUtils.asMap("resource_type", "image"));
+
+        Double durationSeconds = (Double) audioUploadResult.get("duration");
+
+        String duration = formatDuration(durationSeconds);
+
+        Song songToAdd = Song.builder()
+                .name(songRequest.getName())
+                .albumName(songRequest.getAlbum())
+                .image(imageUploadResult.get("secure_url").toString())
+                .file(audioUploadResult.get("secure_url").toString())
+                .duration(duration)
+                .build();
+
+        return songRepository.save(songToAdd);
+    }
+
+    private String formatDuration(Double durationSeconds) {
+        if (durationSeconds == null) {
+            return "0:00";
+        }
+
+        int minutes = (int) (durationSeconds / 60);
+        int seconds = (int) (durationSeconds % 60);
+
+        return String.format("%d:%02d", minutes, seconds);
+    }
+}
