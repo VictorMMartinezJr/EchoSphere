ef4e7c40cebb92644b425794a89e77f729843dd6
Victor <victorm.martinezjr@outlook.com>
2025-10-20 21:55:04 -0400

    create SearchContext

--- frontend/echosphere/src/main.jsx
+++ frontend/echosphere/src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { AuthProvider } from "./context/authContext.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { PlayerContextProvider } from "./context/PlayerContext.jsx";
+import { SearchProvider } from "./context/SearchContext.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <AuthProvider>
+      <PlayerContextProvider>
+        <SearchProvider>
+          <App />
+        </SearchProvider>
+      </PlayerContextProvider>
+    </AuthProvider>
+  </BrowserRouter>
+);
