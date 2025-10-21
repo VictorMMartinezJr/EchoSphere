f2923de1bef33bb48a0121d0c8d3b7efd6c4f94a
Victor <victorm.martinezjr@outlook.com>
2025-10-20 21:39:24 -0400

    add jsx for DisplayAlbum page

---
+++ frontend/echosphere/src/components/DisplayAlbum.jsx
@@ -0,0 +1,69 @@
+import { useContext } from "react";
+import { useParams } from "react-router-dom";
+import { PlayerContext } from "../context/PlayerContext";
+import { assets } from "../assets/assets";
+import { FaRegClock } from "react-icons/fa";
+
+const DisplayAlbum = ({ album }) => {
+  const { albumsData, songsData } = useContext(PlayerContext);
+  const { id } = useParams();
+
+  console.log(songsData);
+
+  return albumsData ? (
+    <>
+      <div className="flex flex-col flex-gap mt-10 gap-8 md:flex-row md:items-end">
+        <img src={album?.imageUrl} alt="Album cover" className="w-48 rounded" />
+        <div className="flex flex-col">
+          <p>Playlist</p>
+          <h2 className="text-5xl font-bold mb-4 md:text-7xl">{album?.name}</h2>
+          <h4>{album?.description}</h4>
+          <p className="mt-1">
+            <img
+              src={assets.logo}
+              alt="EchoSphere logo"
+              className="inline-block w-5 mr-1"
+            />
+            <b>EchoSphere</b> . 1,233,456 . <b>6 Songs .</b> about 2 hr 30min
+          </p>
+        </div>
+      </div>
+
+      <div className="grid grid-cols-3 mt-10 mb-4 pl-2 sm:grid-cols-4 text-[#a7a7a7]">
+        <p>
+          <b className="mr-4">#</b>
+        </p>
+        <p>Album</p>
+        <p className="hidden sm:block">Date Added</p>
+        <div className="m-auto w-4">
+          <FaRegClock />
+        </div>
+      </div>
+
+      <hr />
+
+      {songsData
+        .filter((s) => s.albumName === album.name)
+        .map((song, i) => (
+          <div
+            key={i}
+            className="grid grid-cols-3 gap-2 p-2 items-center text-[#a7a7a7] cursor-pointer hover:bg-[#ffffff2b] sm:grid-cols-4"
+          >
+            <p className="text-white">
+              <b className="mr-4 text-[#a7a7a7]">{++i}</b>
+              <img
+                src={song.image}
+                alt="Song cover"
+                className="inline w-10 mr-5"
+              />
+            </p>
+            <p className="text-[15px]">{song.albumName}</p>
+            <p className="hidden sm:block text-[15px]">5 days ago</p>
+            <p className="text-center text-[15px]">{song.duration}</p>
+          </div>
+        ))}
+    </>
+  ) : null;
+};
+
+export default DisplayAlbum;
