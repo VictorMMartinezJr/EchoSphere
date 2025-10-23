310d97f4fba6e5b7700c50af520ca74e5f31b8f2
Victor <victorm.martinezjr@outlook.com>
2025-10-22 22:43:02 -0400

    add Player component and audio tag to App.jsx

--- frontend/echosphere/src/App.jsx
+++ frontend/echosphere/src/App.jsx
@@ -1,14 +1,34 @@
 import { Toaster } from "react-hot-toast";
-import Login from "./components/Login";
-import Register from "./components/Register";
+import Display from "./components/Display";
+import AuthWrapper from "./components/AuthWrapper";
+import Sidebar from "./components/Sidebar";
+import Player from "./components/Player";
+import { useContext } from "react";
+import { PlayerContext } from "./context/PlayerContext";
 
 const App = () => {
+  const { audioRef, track } = useContext(PlayerContext);
+
   return (
-    <div>
+    <>
       <Toaster />
-      <Login />
-      <Register />
-    </div>
+      <AuthWrapper>
+        <div className="h-screen bg-black">
+          <div className="h-[90%] flex">
+            <Sidebar />
+            <Display />
+          </div>
+
+          {/* Player component */}
+          <Player />
+          <audio
+            ref={audioRef}
+            src={track ? track.file : ""}
+            preload="auto"
+          ></audio>
+        </div>
+      </AuthWrapper>
+    </>
   );
 };
 
