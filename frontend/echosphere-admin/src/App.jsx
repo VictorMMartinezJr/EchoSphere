345795dd3caeb1e4f716c94ebef741a46a329757
Victor <victorm.martinezjr@outlook.com>
2025-11-04 21:30:27 -0500

    create ProtectedRoutes and add to App.jsx

--- frontend/echosphere-admin/src/App.jsx
+++ frontend/echosphere-admin/src/App.jsx
@@ -5,6 +5,7 @@ import AddSong from "./pages/AddSong";
 import ListSongs from "./pages/ListSongs";
 import AddAlbum from "./pages/AddAlbum";
 import ListAlbums from "./pages/ListAlbums";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 export const API_BASE_URL = "http://localhost:8080";
 
@@ -14,12 +15,47 @@ const App = () => {
       <Toaster position="top-center" />
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/add-song" element={<AddSong />} />
-        <Route path="/add-album" element={<AddAlbum />} />
-        <Route path="/list-songs" element={<ListSongs />} />
-        <Route path="/list-albums" element={<ListAlbums />} />
+        <Route
+          path="/add-song"
+          element={
+            <ProtectedRoute requiredAdmin={true}>
+              <AddSong />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/add-album"
+          element={
+            <ProtectedRoute requiredAdmin={true}>
+              <AddAlbum />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/list-songs"
+          element={
+            <ProtectedRoute requiredAdmin={true}>
+              <ListSongs />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/list-albums"
+          element={
+            <ProtectedRoute requiredAdmin={true}>
+              <ListAlbums />
+            </ProtectedRoute>
+          }
+        />
         <Route path="/" element={<Login />} />
-        <Route path="/*" element={<AddSong />} />
+        <Route
+          path="/*"
+          element={
+            <ProtectedRoute requiredAdmin={true}>
+              <AddSong />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
--- frontend/echosphere-admin/src/components/ProtectedRoute.jsx
+++ frontend/echosphere-admin/src/components/ProtectedRoute.jsx
@@ -1,5 +1,35 @@
-const ProtectedRoute = () => {
-  return <div>ProtectedRoute</div>;
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+const ProtectedRoute = ({ children, requiredAdmin = false }) => {
+  const { isAuthenticated, isAdmin, loading } = useAuth();
+
+  if (loading) {
+    return (
+      <div className="min-h-screen bg-gradient-to-br from-blue-500 via-black to-pink-500 flex justify-center items-center">
+        <div className="text-center">
+          <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-pink-500 mx-auto mb-4"></div>
+          <p className="text-white text-lg">Loading...</p>
+        </div>
+      </div>
+    );
+  }
+
+  if (!isAuthenticated()) {
+    return <Navigate to="/login" replace />;
+  }
+
+  if (requiredAdmin && !isAdmin()) {
+    return (
+      <div className="min-h-screen bg-gradient-to-br  from-blue-500 via-black to-pink-500 flex justify-center items-center">
+        <div className="text-center">
+          <p className="text-white text-2xl font-bold">Access Denied</p>
+          <p className="text-white text-lg">Admin's only.</p>
+        </div>
+      </div>
+    );
+  }
+
+  return children;
 };
 
 export default ProtectedRoute;
