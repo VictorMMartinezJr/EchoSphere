ce3b7ad796dfb84b6059a0bab612020f3bd28b10
Victor <victorm.martinezjr@outlook.com>
2025-11-06 23:51:40 -0500

    create Dashboard component and add Sidebar

--- frontend/echosphere-admin/src/layout/Dashboard.jsx
+++ frontend/echosphere-admin/src/layout/Dashboard.jsx
@@ -1,5 +1,18 @@
-const Dashboard = () => {
-  return <div>Dashboard</div>;
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+
+const Dashboard = ({ children, activeMenu }) => {
+  return (
+    <div>
+      <Navbar activeMenu={activeMenu} />
+      <div className="flex">
+        <div className="max-[1024px]:hidden">
+          <Sidebar activeMenu={activeMenu} />
+        </div>
+        <div className="grow mx-5">{children}</div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
