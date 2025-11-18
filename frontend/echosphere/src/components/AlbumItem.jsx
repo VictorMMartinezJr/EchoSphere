58f6a0e6978fb1632c5070b424add79062fbfe02
Victor <victorm.martinezjr@outlook.com>
2025-11-18 17:34:03 -0500

    fix image height on Album and Song item

--- frontend/echosphere/src/components/AlbumItem.jsx
+++ frontend/echosphere/src/components/AlbumItem.jsx
@@ -8,7 +8,11 @@ const AlbumItem = ({ id, name, desc, image }) => {
       className="min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]"
       onClick={() => navigate(`/album/${id}`)}
     >
-      <img src={image} alt="album cover" className="rounded" />
+      <img
+        src={image}
+        alt="album cover"
+        className="rounded object-contain h-50"
+      />
       <p className="font-bold mt-2 mb-1">{name}</p>
       <p className="text-slate-200 text-sm">{desc}</p>
     </div>
--- frontend/echosphere/src/components/SongItem.jsx
+++ frontend/echosphere/src/components/SongItem.jsx
@@ -1,7 +1,11 @@
-const SongItem = ({ id, name, image }) => {
+const SongItem = ({ name, image }) => {
   return (
-    <div className="min-w-[180px] p-2 py-3 rounded cursor-pointer hover:bg-[#ffffff26]">
-      <img src={image} alt="Song cover" className="rounded" />
+    <div className="min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]">
+      <img
+        src={image}
+        alt="album cover"
+        className="rounded object-contain h-50"
+      />
       <p className="font-bold mt-2 mb-1">{name}</p>
     </div>
   );
